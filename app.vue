1d6bc2728b141d84f797f9c4c682b29dde20f812
zq19920306 <1113638674@qq.com>
2023-04-09 00:58:55 +0800

    update:安装依赖

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="page">
+    <h1 class="title">Hello World</h1>
+    <el-button type="primary">测试</el-button>
   </div>
 </template>
+
+<script lang="ts" setup>
+useHead({
+  title: '一梦浮生'
+})
+</script>
+
+<style lang="less" scoped>
+.page {
+  .title {
+    font-size: 28px;
+  }
+}
+</style>
